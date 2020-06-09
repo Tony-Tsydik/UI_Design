e48c52c671212302526544b60c1395aa7536c5d3
Tony <tsyda1995@mail.ru>
2020-06-09 18:29:50 +0300

    v.2

--- averageAge/averageAge.js
+++ averageAge/averageAge.js
@@ -1,4 +1,4 @@
-const middleAgeBySex = (infoArray, sex) => {
+/*const middleAgeBySex = (infoArray, sex) => {
 	let persons = [];
  
  	if (sex=='M') {
@@ -42,4 +42,57 @@ const infoArray = [{
         },
 ];
 
+middleAgeBySex (infoArray, `W`);*/
+
+
+const middleAgeBySex = (infoArray, sex) => {
+    let menAge = 0,
+    numberOfMen = 0,
+    numberOfWomen = 0,
+    womenAge = 0,
+    result = 0;
+    infoArray.forEach ((item) => {
+    if (item.sex == 'M'){
+    numberOfMen += 1;
+    menAge += item.age;
+} else {numberOfWomen += 1;
+    womenAge += item.age};
+});
+    (sex == 'M')? result = menAge/numberOfMen : result = womenAge/numberOfWomen;
+    alert(result);
+}
+
+const infoArray = [{
+            name: `Ivan`,
+            sex: `M`,
+            age: 21
+        },
+        {
+            name: `Olga`,
+            sex: `W`,
+            age: 19
+            },
+        {
+            name: `Oleg`,
+            sex: `M`,
+            age: 27
+        },
+        {
+            name: `Ann`,
+            sex: `W`,
+            age: 21
+        },
+        {
+            name: `Alex`,
+            sex: `M`,
+            age: 30
+        },
+];
+
 middleAgeBySex (infoArray, `W`);
+
+
+
+
+
+
